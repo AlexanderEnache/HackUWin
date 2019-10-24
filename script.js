100e17b47cf3f3f1e5a2a72d2b61e5f08c7aeecb
Alex Enache <ragnarxon@gmail.com>
2019-10-24 12:41:35 -0400

    made smooth scroll

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 
 $(document).ready(function (){
-     $("#click").click(function (){
-       console.log("Stuff");
-         $('html, body').animate({
-             scrollTop: $("#faq").offset().top
-         }, 1000);
-     });
+    $("#about-scroll").click(function (){
+      console.log("Stuff");
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 1000);
+    });
 });
+
+$(document).ready(function (){
+    $("#sponsor-scroll").click(function (){
+      console.log("Stuff");
+        $('html, body').animate({
+            scrollTop: $("#sponsor").offset().top
+        }, 1000);
+    });
+});
+
+$(document).ready(function (){
+    $("#faq-scroll").click(function (){
+      console.log("Stuff");
+        $('html, body').animate({
+            scrollTop: $("#faq").offset().top
+        }, 1000);
+    });
+});
+
  
 
